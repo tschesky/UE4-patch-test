215eaa36296a091c4d0233df976d7a308835aca2
Michal <Michal@Eddaheim.com>
2022-05-09 11:03:06 +0200

    Init project

---
+++ Source/PatchTest/PatchTest.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PatchTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PatchTest, "PatchTest" );
+ 
